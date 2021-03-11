2865d1f3bac094aacafe5c1af20bb713ca3dcab6
Medina Pilavdzic <medina.pilavdzic@gmail.com>
2021-03-11 21:05:09 +0100

    Staged in VSC

---
+++ ex01/src/index.js
@@ -0,0 +1,23 @@
+my Res = 18 / 9; // assigned 2
+
+function main(numOne, numTwo) {
+    var quotient;
+    if (numTwo === 0) {
+        quotient = 'Divide by 0! Cannot divide by zero!';
+    } else {
+        // Onlx change code below this line
+        quotient = numOne / numTwo;
+        //expected output: 5, 5, 42, cannot divide by zero!
+
+        // Only change code above this line
+
+    }
+    return quotient;
+}
+
+
+console.log(main(15, 3));
+console.log(main(25, 5));
+console.log(main(420, 10));
+console.log(main(55, 0)); // change this line
+modul.exports = main;
---
+++ ex02/src/index.js
@@ -0,0 +1,19 @@
+function main(numOne, numTwo) {
+    var my Res = 75;
+    if (numTwo === 0) {
+
+        // Onlx change code below this line
+        let my Res = 75;
+        const y = x++;
+
+        console.log(main(`x:${x}, y:${y}`));
+
+        // Only change code above this line
+
+    }
+    return myRes;
+}
+
+
+console.log(main());
+modul.exports = main;
